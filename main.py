5fcbfa44a63edc40e736dd46914f5e7a6096afb0
Arran <plazmapie335@gmail.com>
2022-04-16 21:24:17 +0200

    Simple scatter plot
    Simple plot of distance and cost of the trips to show that there is a clear coloration between the two.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 try:
     fareData = pd.read_csv('./data/sampleData/sampleDataFare.csv')
@@ -7,4 +8,16 @@ try:
 except:
     print("Error [Check data exists in file location]")
 
-print(fareData)
+totalAmountDataFrame = fareData[' total_amount']
+distanceOfTrip = tripData['trip_distance']
+
+plt.figure(figsize=(6, 6))
+plt.scatter(distanceOfTrip, totalAmountDataFrame)
+plt.xlabel('Distance', fontsize=14, labelpad=15)
+plt.ylabel('Cost', fontsize=14, labelpad=15)
+plt.show()
+
+
+# scatter plot for distance and money earned
+
+

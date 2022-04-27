0fb46a87e607ec64cb738fd9bb28791393899a56
Arran <plazmapie335@gmail.com>
2022-04-27 22:26:49 +0200

    Bug fixed
    Changed from medallion merger to index merger.

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ def calcAverage(statement, amount, rounder, unit):
     print(f"{statement} {round(average, rounder)} {unit}")
 
 
-def calcProbability(y, z, message):
+def calcProbability(y, z, message, rounder):
     print(y,z)
     prob = y / z
-    print(f'{message}{round(prob, 3)}')
+    print(f'{message}{round(prob, rounder)}')
 
 
 # Trips Above 50km
 distanceAbove50 = tripData.loc[tripData.trip_distance >= 50]
 # Merge the medallion so I can access financial data
-distanceAbove50 = distanceAbove50.merge(fareData, left_on="medallion", right_on="medallion", how="left")
+distanceAbove50 = distanceAbove50.merge(fareData, left_index=True, right_index=True)
 distanceAbove50.to_csv("./data/dataOutput/tripsAbove50.csv")
 
 # Trips below 30km
 distanceBelow30 = tripData.loc[tripData.trip_distance <= 30]
 # Merge medallion so both cost and distance are available
-distanceBelow30 = distanceBelow30.merge(fareData, left_on="medallion", right_on="medallion", how="left")
+distanceBelow30 = distanceBelow30.merge(fareData, left_index=True, right_index=True)
 distanceBelow30.to_csv("./data/dataOutput/tripsBelow30.csv")
 
 # Trips above 50km
 distanceBelow50km = tripData.loc[tripData.trip_distance <= 50]
-distanceBelow50km = distanceBelow50km.merge(fareData, left_on="medallion", right_on="medallion", how="left")
+distanceBelow50km = distanceBelow50km.merge(fareData, left_index=True, right_index=True)
 distanceBelow50km.to_csv("./data/dataOutput/tripsBelow50.csv")
 
 # Select trips that are above 30km in length
 distanceAbove30 = tripData.loc[tripData.trip_distance >= 30]
 # Merge the data by medallion to create a dataset with both trips >= 30 km and the cost of the trip
-distanceAbove30 = distanceAbove30.merge(fareData, left_on="medallion", right_on="medallion", how="left")
+distanceAbove30 = distanceAbove30.merge(fareData, left_index=True, right_index=True)
 distanceAbove30.to_csv("./data/dataOutput/tripsAbove30.csv")
 
 totalAmountDataFrame = fareData[' total_amount']
@@ -54,8 +54,8 @@ calcAverage('Average distance for ALL trip', distanceOfTrip, 3, 'km')
 calcAverage('Average cost for trips below 30 km: $', distanceBelow30Cost, 3, 'USD')
 calcAverage('Average cost for trips below 50 km: $', distanceBelow50kmCost, 3, 'USD')
 calcAverage('Average cost for trips Above 30 km: $', distanceAbove30Cost, 3, 'USD')
-calcProbability(len(distanceAbove30), len(tripData), 'Probability of trip being above 30km: %')
-calcProbability(len(distanceBelow30), len(tripData), 'Probability of trip being below 30km: %')
+calcProbability(len(distanceAbove30), len(tripData), 'Probability of trip being above 30km: %', 5)
+calcProbability(len(distanceBelow30), len(tripData), 'Probability of trip being below 30km: %', 5)
 
 # scatter plot for distance and money earned
 plt.figure(figsize=(6, 6))
